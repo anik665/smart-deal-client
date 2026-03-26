e6c3dfbd157e7651945464fbda252eda4f6cbbef
Anik Ghosh <anikgh7@gmail.com>
2026-03-26 17:37:33 +0600

    card design start

--- src/components/LatestProducts/LatestProducts.jsx
+++ src/components/LatestProducts/LatestProducts.jsx
@@ -1,8 +1,18 @@
-import React from "react";
+import React, { use } from "react";
+import Product from "../Product/Product";
 
 const LatestProducts = ({ latestproducts }) => {
-  console.log(latestproducts);
-  return <div></div>;
+  const products = use(latestproducts);
+  console.log(products);
+  return (
+    <div>
+      <div className="grid grid-cols-3">
+        {products.map((product) => (
+          <Product key={product._id} product={product}></Product>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default LatestProducts;
